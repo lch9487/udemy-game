4ba109a27578396b98361146388cc809fc262734
lch9487 <nelson81411@gmail.com>
2019-01-15 14:23:51 +0800

    added storybook settings

---
+++ stories/Header.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import Header from '../components/Header';
+
+storiesOf('Header', module).add('Header', () => <Header />);
